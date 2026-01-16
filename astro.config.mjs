b1df27a8c21e8952f8ed07c62bb5653cc2bd3556
Agung Adhinata <agung-adhinata@proton.me>
2026-01-17 02:26:22 +0800

    fix: config error

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,10 @@ import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://astro.build/config
 export default defineConfig({
